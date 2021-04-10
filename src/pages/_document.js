5921e7d8ff17be78e2ec944a00e2acb4407c2b36
ConsuTI <guilhermereis9876@gmail.com>
2021-04-09 23:59:01 -0300

    feat: google sc

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
             name="description"
             content="Inicie conversas no whatsapp sem adicionar aos contatos."
           />
-
+          ''
           <meta property="og:type" content="website" />
           <meta
             property="og:url"
@@ -24,7 +24,6 @@ export default class MyDocument extends Document {
             property="og:description"
             content="Inicie conversas no whatsapp sem adicionar aos contatos."
           />
-
           <meta property="twitter:card" content="summary_large_image" />
           <meta
             property="twitter:url"
@@ -35,14 +34,16 @@ export default class MyDocument extends Document {
             property="twitter:description"
             content="Inicie conversas no whatsapp sem adicionar aos contatos."
           />
-
           <link rel="shortcut icon" href="/favicon.png" />
-
           <script
             data-ad-client="ca-pub-4785613682714050"
             async
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
           ></script>
+          <meta
+            name="google-site-verification"
+            content="Vny8a2HsVLMIljSHxHCM4BAyFS7khyc3hNu7NlG-rvA"
+          />
           <script src="https://www.gstatic.com/firebasejs/8.3.3/firebase-app.js"></script>
           <script src="https://www.gstatic.com/firebasejs/8.3.3/firebase-analytics.js"></script>
           <script src="/scripts/firebase.js"></script>
